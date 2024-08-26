5f1fde29165cdcb8dcda9fd3f8e276f8319ba52c
lohjal001 <113910139+lohjal001@users.noreply.github.com>
2024-08-26 13:45:12 +0200

    first

---
+++ app/src/main/java/com/example/gritappone/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.gritappone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                button.setBackgroundColor(Color.rgb(03,76, 56));
+                button.setText("clicked");
+                new Toast(setTitle("Toast!"););
+
+            }
+        });
+    }
+}
